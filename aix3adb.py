bc51d3de1fe536d867c0c738ce42206a4ed571bc
Mark <olschew@cern.ch>
2014-10-14 16:40:20 +0200

    search feature

--- aix3adb.py
+++ aix3adb.py
@@ -49,8 +49,10 @@ class aix3adb:
     def editMCSample(self, id, sample):
         check = self.checksample(sample)
         if check:
-            s = self.getAuthServerProxy()
-            return s.editMCSample(id, sample)
+            return self.tryServerAuthFunction(self.doEditMCSample, id, sample)
+    def doEditMCSample(self, id, sample):
+        s = self.getAuthServerProxy()
+        return s.editMCSample(id, sample)
     def insertOrReplaceMCTags(self, id, tags):
         s = self.getAuthServerProxy()
         return s.insertOrReplaceMCTags(id, tags)
@@ -61,12 +63,25 @@ class aix3adb:
     def doRegisterDataSample(self, sample):
         s = self.getAuthServerProxy()
         return s.registerDataSample(sample)
+    def editDataSample(self, id, sample):
+        check = self.checksample(sample)
+        if check:
+            return self.tryServerAuthFunction(self.doEditDataSample, id, sample)
+    def doEditDataSample(self, id, sample):
+        s = self.getAuthServerProxy()
+        return s.editDataSample(id, sample)
     def getMCSample(self, sampleid):
         s = xmlrpclib.ServerProxy(self.readurl)
         return s.getMCSample(sampleid)
     def getDataSample(self, sampleid):
         s = xmlrpclib.ServerProxy(self.readurl)
         return s.getDataSample(sampleid)
+    def searchMCSamples(self, searchdict):
+        s = xmlrpclib.ServerProxy(self.readurl)
+        return s.searchMCSamples(searchdict)
+    def searchDataSamples(self, searchdict):
+        s = xmlrpclib.ServerProxy(self.readurl)
+        return s.searchDataSamples(searchdict)
     def test(self):
         s = self.getAuthServerProxy()
         return s.test()
@@ -81,13 +96,22 @@ class aix3adb:
             log.error("Sample failed sanity checks: " + msg)
             return False
         return True
-    def tryServerAuthFunction(self, funct, sample):
+    def tryServerAuthFunction(self, funct, *params):
         try:
-            return funct(sample)
+            return funct(*params)
         except xmlrpclib.ProtocolError:
             self.destroyauth()
             self.authorize()
-            return funct(sample)
+            return funct(*params)
+
+
+class aix3adbAuth(aix3adb):
+   def __init__(self,username=None, trykerberos=3):
+      aix3adb.__init__(self)
+      self.authorize(username, trykerberos)
+   def __del__(self):
+      self.destroyauth()
+
 
 def main():
     print "This is just a library"
@@ -96,11 +120,11 @@ def main():
 
 class cookietransportrequest:
     """A Transport request method that retains cookies over its lifetime.
- 
+
     The regular xmlrpclib transports ignore cookies. Which causes
     a bit of a problem when you need a cookie-based login, as with
     the Bugzilla XMLRPC interface.
- 
+
     So this is a helper for defining a Transport which looks for
     cookies being set in responses and saves them to add to all future
     requests.
@@ -113,7 +137,7 @@ class cookietransportrequest:
     # Note this must be an old-style class so that __init__ handling works
     # correctly with the old-style Transport class. If you make this class
     # a new-style class, Transport.__init__() won't be called.
- 
+
     cookies = []
     def setcookies(self,cookiefile, domain):
         self.cookies = []
@@ -126,21 +150,21 @@ class cookietransportrequest:
         if self.cookies:
             for cookie in self.cookies:
                 connection.putheader("Cookie", cookie)
- 
+
     def request(self, host, handler, request_body, verbose=0):
         self.verbose = verbose
- 
+
         # issue XML-RPC request
         h = self.make_connection(host)
         if verbose:
             h.set_debuglevel(1)
- 
+
         self.send_request(h, handler, request_body)
         self.send_host(h, host)
         self.send_cookies(h)
         self.send_user_agent(h)
         self.send_content(h, request_body)
- 
+
         # Deal with differences between Python 2.4-2.6 and 2.7.
         # In the former h is a HTTP(S). In the latter it's a
         # HTTP(S)Connection. Luckily, the 2.4-2.6 implementation of
@@ -150,27 +174,27 @@ class cookietransportrequest:
             response = h.getresponse()
         except AttributeError:
             response = h._conn.getresponse()
- 
+
         # Add any cookie definitions to our list.
         for header in response.msg.getallmatchingheaders("Set-Cookie"):
             val = header.split(": ", 1)[1]
             cookie = val.split(";", 1)[0]
             self.cookies.append(cookie)
- 
+
         if response.status != 200:
             raise xmlrpclib.ProtocolError(host + handler, response.status,
                                           response.reason, response.msg.headers)
- 
+
         payload = response.read()
         parser, unmarshaller = self.getparser()
         parser.feed(payload)
         parser.close()
- 
+
         return unmarshaller.close()
- 
+
 class cookietransport(cookietransportrequest, xmlrpclib.Transport):
     pass
- 
+
 class cookiesafetransport(cookietransportrequest, xmlrpclib.SafeTransport):
     pass
 
@@ -184,6 +208,7 @@ def transport(uri):
         return cookiesafetransport()
     else:
         return cookietransport()
-    
+
 if __name__ == "__main__":
     main()
+
--- aix3adbtest.py
+++ aix3adbtest.py
@@ -6,40 +6,41 @@ def main():
 
     dblink=aix3adb.aix3adb()
     # Ein Sample holen
-    result = dblink.getMCSample(11)
+    result = dblink.searchMCSamples({"original_name":"/WJetsToLNu_M-1000_TuneZ2star_8TeV-madgraph/Summer12-START53_V19_FSIM/AODSIM"})
+    #result = dblink.getMCSample(11)
     pp.pprint(result)
     
     
-    #Ein Sample erstellen
-    files=["/neuertag","1/2/3.root"]
-    tags = dict()
-    tags['MUSiC_test']="blub2";
-    tags['MUSiC_test2']="blub3";
-    sample = dict()
-    sample['original_name'] = "/neuertag"
-    sample['skimmer_name'] = "CMS"
-    sample['skimmer_version'] = "1.23.4"
-    sample['skimmer_cmssw'] = "CMSSW_1_2_3"
-    sample['energy'] = 8
-    sample['crosssection'] = 1.234
-    sample['crosssection_reference'] = ""
-    sample['kfactor'] = 1
-    sample['kfactor_reference'] = ""
-    sample['prep_key'] = 0
-    sample['parentsample'] = 0
-    sample = dict()
-    sample['description'] = "bla"
-    #sample['files'] = files
-    #sample['tags'] = tags
+    ##Ein Sample erstellen
+    #files=["/neuertag","1/2/3.root"]
+    #tags = dict()
+    #tags['MUSiC_test']="blub2";
+    #tags['MUSiC_test2']="blub3";
+    #sample = dict()
+    #sample['original_name'] = "/neuertag"
+    #sample['skimmer_name'] = "CMS"
+    #sample['skimmer_version'] = "1.23.4"
+    #sample['skimmer_cmssw'] = "CMSSW_1_2_3"
+    #sample['energy'] = 8
+    #sample['crosssection'] = 1.234
+    #sample['crosssection_reference'] = ""
+    #sample['kfactor'] = 1
+    #sample['kfactor_reference'] = ""
+    #sample['prep_key'] = 0
+    #sample['parentsample'] = 0
+    #sample = dict()
+    #sample['description'] = "bla"
+    ##sample['files'] = files
+    ##sample['tags'] = tags
 
-    #Zum Schreiben muss man sich zunaechst autorisieren (lesen geht auch ohne)
-    dblink.authorize(username="olschew")
-    neuessample = dblink.test()
-    print sample
-    print "-----"
-    neuessample = dblink.registerMCSample(sample)
-    #neuessample = dblink.insertOrReplaceMCTags(2,tags)
-    print neuessample
-    dblink.destroyauth()
+    ##Zum Schreiben muss man sich zunaechst autorisieren (lesen geht auch ohne)
+    #dblink.authorize(username="olschew")
+    #neuessample = dblink.test()
+    #print sample
+    #print "-----"
+    #neuessample = dblink.registerMCSample(sample)
+    ##neuessample = dblink.insertOrReplaceMCTags(2,tags)
+    #print neuessample
+    #dblink.destroyauth()
 if __name__=="__main__":
     main()
